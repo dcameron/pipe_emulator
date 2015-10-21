8cf96c86c0b5977149fcfccea92bc377acefbc2e
David Cameron <david@cadeyrn.us>
2015-10-21 14:02:57 -0500

    Added dynamic offsets to PipeEmulator\PipeUI.

--- src/PipeEmulator/PipeUI.php
+++ src/PipeEmulator/PipeUI.php
@@ -37,7 +37,7 @@ class PipeUI {
    * @param \PipeEmulator\Pipe $pipe
    *   The pipe to be displayed by the user interface.
    */
-  function __construct(\PipeEmulator\Pipe $pipe) {
+  public function __construct(\PipeEmulator\Pipe $pipe) {
     $this->pipe = $pipe;
   }
 
@@ -47,12 +47,14 @@ class PipeUI {
    * @return string
    *   The rendered HTML of the pipe.
    */
-  function renderPipe() {
+  public function renderPipe() {
     $layout = $this->pipe->getLayout();
+    list($x_offset, $y_offset) = $this->getCoordinateOffsets($layout);
+
     $output = '<style>.module { background: #EEEEEE; border: 1px solid #000000; border-radius: 3px; padding: 2px; position: absolute; width: 200px;} .module-label { font-weight: bold; }</style>';
 
     foreach ($this->pipe->getModules() as $id => $module) {
-      $output .= '<div class="module" style="top: ' . ($layout[$id]['y'] + 100) . 'px; left: ' . ($layout[$id]['x'] + 100) . 'px;">';
+      $output .= '<div class="module" style="top: ' . ($layout[$id]['y'] + $y_offset) . 'px; left: ' . ($layout[$id]['x'] + $x_offset) . 'px;">';
       $output .= '<div class="module-label">' . $module->getLabel() . '</div>';
       $content = $module->getContent();
       if ($content != '') {
@@ -61,5 +63,38 @@ class PipeUI {
       $output .= '</div>';
     }
     return $output;
+  }
+
+  /**
+   * Returns offsets to add to the x and y coordinates of modules in the pipe.
+   *
+   * In Yahoo! Pipes, the coordinates of modules could have negative values. If
+   * those negative values are applied to the positions of HTML elements, then
+   * they will be rendered off-screen. Find the lowest negative values and
+   * return them so they can be added used to offset the positions of the
+   * modules.
+   *
+   * @param array $layout
+   *   An array of module position coordinates.
+   *
+   * @return array
+   *   Element 0 contains the x-axis offset. Element 1 contains the y-axis
+   *   offset.
+   */
+  protected function getCoordinateOffsets($layout) {
+    $x_offset = 0;
+    $y_offset = 0;
+
+    foreach ($layout as $module) {
+      if ($module['x'] < $x_offset) {
+        $x_offset = (int) $module['x'];
+      }
+      if ($module['y'] < $y_offset) {
+        $y_offset = (int) $module['y'];
+      }
+    }
+
+    // Add an extra 10px to the offset values for some padding.
+    return array(abs($x_offset) + 10, abs($y_offset) + 10);
   }
 }
