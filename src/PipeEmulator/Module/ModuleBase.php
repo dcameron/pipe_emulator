76d9c9f76381978755733d85bb5e8b96a43d7cef
David Cameron <david@cadeyrn.us>
2015-10-20 22:40:52 -0500

    Wrote documentation for PipeEmulator\Module\ModuleBase.

--- src/PipeEmulator/Module/ModuleBase.php
+++ src/PipeEmulator/Module/ModuleBase.php
@@ -8,25 +8,48 @@
 namespace PipeEmulator\Module;
 
 /**
- * Description of ModuleBase
- *
- * @author David Cameron
+ * Defines a base pipe module class.
  */
 abstract class ModuleBase implements ModuleInterface {
 
+  /**
+   * The module's ID.
+   *
+   * @var string 
+   */
   protected $id;
 
+  /**
+   * An array of module IDs to which the output of this module should be sent.
+   *
+   * @var array 
+   */
   protected $outputs = array();
 
+  /**
+   * Constructs a pipe module object.
+   *
+   * @param stdClass $module_definition
+   *   An object representing a module that was extracted from a decoded Yahoo!
+   *   Pipes definition JSON object.
+   * @param array $outputs
+   *   An array of module IDs.
+   */
   public function __construct($module_definition, $outputs) {
     $this->id = $module_definition->id;
     $this->outputs = $outputs;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getOutputs() {
     return $this->outputs;
   }
 
+  /**
+   * Returns the $input array with no additional processing.
+   */
   public function evaluateInput($input) {
     return $input;
   }
