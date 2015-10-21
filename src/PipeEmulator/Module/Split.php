1ff83fb4905aecd27944412f1fb2136131c0b468
David Cameron <david@cadeyrn.us>
2015-10-20 22:59:02 -0500

    Wrote documentation for PipeEmulator\Module\Split.

--- src/PipeEmulator/Module/Split.php
+++ src/PipeEmulator/Module/Split.php
@@ -8,12 +8,13 @@
 namespace PipeEmulator\Module;
 
 /**
- * Description of output
- *
- * @author David Cameron
+ * Defines a split pipe module class.
  */
 class Split extends ModuleBase implements ModuleInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct($module_definition, $outputs) {
     parent::__construct($module_definition, $outputs);
   }
