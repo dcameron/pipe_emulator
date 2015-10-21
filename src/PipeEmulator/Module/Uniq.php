7c029828c09418c76d9e47958ac4a27a21299acc
David Cameron <david@cadeyrn.us>
2015-10-20 23:03:14 -0500

    Wrote documentation for PipeEmulator\Module\Uniq.

--- src/PipeEmulator/Module/Uniq.php
+++ src/PipeEmulator/Module/Uniq.php
@@ -8,12 +8,15 @@
 namespace PipeEmulator\Module;
 
 /**
- * Description of output
+ * Defines a uniq pipe module class.
  *
- * @author David Cameron
+ * In Yahoo! Pipes uniq modules removed duplicate values from their input.
  */
 class Uniq extends ModuleBase implements ModuleInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct($module_definition, $outputs) {
     parent::__construct($module_definition, $outputs);
   }
