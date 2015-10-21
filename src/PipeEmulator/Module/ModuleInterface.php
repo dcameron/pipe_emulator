d47b32cd721dbf5550eb5ab1493ea2ac82914985
David Cameron <david@cadeyrn.us>
2015-10-20 22:11:56 -0500

    Wrote documentation for PipeEmulator\Module\ModuleInterface.

--- src/PipeEmulator/Module/ModuleInterface.php
+++ src/PipeEmulator/Module/ModuleInterface.php
@@ -8,12 +8,27 @@
 namespace PipeEmulator\Module;
 
 /**
- *
- * @author David Cameron
+ * Provides an interface for defining a pipe module.
  */
 interface ModuleInterface {
 
+  /**
+   * Gets the outputs of the module.
+   *
+   * @return array
+   *   Returns the value of the $outputs property.
+   */
   public function getOutputs();
 
+  /**
+   * Performs some processing task on the input.
+   *
+   * @param array $input
+   *   An associative array of values to be processed.
+   *
+   * @return FALSE|array
+   *   FALSE if the input should not be passed to the next module(s) in the
+   *   pipe.  Array containing the results of the processed input.
+   */
   public function evaluateInput($input);
 }
