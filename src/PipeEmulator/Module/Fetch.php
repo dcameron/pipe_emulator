1f416b2ac185ac09b573a1d09676b779cb26290b
David Cameron <david@cadeyrn.us>
2015-10-20 23:12:47 -0500

    Added more documentation to PipeEmulator\Module\Fetch.

--- src/PipeEmulator/Module/Fetch.php
+++ src/PipeEmulator/Module/Fetch.php
@@ -9,6 +9,9 @@ namespace PipeEmulator\Module;
 
 /**
  * Defines a fetch pipe module class.
+ *
+ * In Yahoo! Pipes fetch modules were the starting points of pipes. They fetched
+ * content from a source providing input for the pipe.
  */
 class Fetch extends ModuleBase implements ModuleInterface {
 
