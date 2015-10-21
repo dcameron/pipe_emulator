ecc55c46adf1bb37487589b235bbbcaef0a84006
David Cameron <david@cadeyrn.us>
2015-10-21 13:18:36 -0500

    Added the renderPipe method to PipeEmulator\PipeUI.

--- src/PipeEmulator/PipeUI.php
+++ src/PipeEmulator/PipeUI.php
@@ -28,4 +28,26 @@ class PipeUI {
   function __construct(\PipeEmulator\Pipe $pipe) {
     $this->pipe = $pipe;
   }
+
+  /**
+   * Builds an HTML representation of the pipe's modules.
+   *
+   * @return string
+   *   The rendered HTML of the pipe.
+   */
+  function renderPipe() {
+    $layout = $this->pipe->getLayout();
+    $output = '<style>.module { background: #EEEEEE; border: 1px solid #000000; border-radius: 3px; padding: 2px; position: absolute; width: 200px;} .module-label { font-weight: bold; }</style>';
+
+    foreach ($this->pipe->getModules() as $id => $module) {
+      $output .= '<div class="module" style="top: ' . ($layout[$id]['y'] + 100) . 'px; left: ' . ($layout[$id]['x'] + 100) . 'px;">';
+      $output .= '<div class="module-label">' . $module->getLabel() . '</div>';
+      $content = $module->getContent();
+      if ($content != '') {
+        $output .= $content;
+      }
+      $output .= '</div>';
+    }
+    return $output;
+  }
 }
