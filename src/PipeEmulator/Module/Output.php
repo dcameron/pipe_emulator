2051ba6fefca853497a6ac28c02200c2a51933cb
David Cameron <david@cadeyrn.us>
2015-10-20 23:11:05 -0500

    Added more documentation to PipeEmulator\Module\Output.

--- src/PipeEmulator/Module/Output.php
+++ src/PipeEmulator/Module/Output.php
@@ -9,6 +9,8 @@ namespace PipeEmulator\Module;
 
 /**
  * Defines an output pipe module class.
+ *
+ * In Yahoo! Pipes output modules were the end point of the pipe.
  */
 class Output extends ModuleBase implements ModuleInterface {
 
